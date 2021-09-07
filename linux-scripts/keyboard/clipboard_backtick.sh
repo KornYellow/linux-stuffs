aaa3ee6a166739c3517740ff0dd05068d7833ceb
Korn <kornkubzaza@gmail.com>
2021-09-07 14:18:19 +0700

    Update script

--- linux-scripts/keyboard/clipboard_backtick.sh
+++ linux-scripts/keyboard/clipboard_backtick.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Copy backtick to clipboard
-# sleep 0.2
-# sh -c "echo -n '\`' | xclip -select clipboard"
+sleep 0.2
+sh -c "echo -n '\`' | xclip -select clipboard"
 
 # Send notify
-# sleep 0.2
-# notify-send 'Clipboard' 'backtick was saved to the clipboard.'
+sleep 0.2
+notify-send 'Clipboard' 'backtick was saved to your clipboard.'
