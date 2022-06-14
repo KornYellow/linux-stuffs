4bd329f55c76d4cee90343d00c90aef5338b3283
kornyellow <kornkubzaza@gmail.com>
2022-06-15 06:03:23 +0700

    Increase delay before copying password

--- linux-scripts/application/open-line-and-login.sh
+++ linux-scripts/application/open-line-and-login.sh
@@ -5,10 +5,11 @@ sleep 0.2
 brave chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html &
 
 # Script that copy password from
+sleep 0.5
 bash /home/korn/git/linux-stuffs/credential/password.sh &
 
 # Type Password
-sleep 1.5
+sleep 0.5
 xdotool getactivewindow key --clearmodifiers ctrl+v &
 sleep 0.5
 xdotool getactivewindow key --clearmodifiers Enter &
